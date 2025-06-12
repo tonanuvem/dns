bad71d9fa104a63147e022ca602dfa3af2d77014
tonanuvem <contato@tonanuvem.net>
2025-06-12 19:37:46 -0300

    atualizando

--- terraform/modules/lambda_api/main.tf
+++ terraform/modules/lambda_api/main.tf
@@ -1,7 +1,7 @@
 # Arquivo ZIP da função Lambda
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../lambda_zip"
+  source_file = "${path.root}/lambda_zip/gerenciador_dns.zip"
   output_path = "${path.module}/lambda_function.zip"
 }
 
