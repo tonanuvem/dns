63fb35da7304f92abd102f25b3be339c0c377606
tonanuvem <contato@tonanuvem.net>
2025-06-12 14:05:17 -0300

    atualizando

--- config.sh
+++ config.sh
@@ -12,6 +12,29 @@ SENHA="$2"
 # Navega até a pasta scripts
 cd scripts || { echo "Erro: pasta 'scripts' não encontrada"; exit 1; }
 
+# Verifica se o venv existe, se não, cria
+if [ ! -d "venv" ]; then
+    echo "Criando ambiente virtual..."
+    python3 -m venv venv
+fi
+
+# Ativa o venv
+echo "Ativando ambiente virtual..."
+source venv/bin/activate
+
+# Instala/atualiza pip
+python -m pip install --upgrade pip
+
+# Verifica se requirements.txt existe
+if [ -f "requirements.txt" ]; then
+    echo "Instalando dependências..."
+    pip install -r requirements.txt
+else
+    echo "Arquivo requirements.txt não encontrado"
+    exit 1
+fi
+
+
 # Torna o script configurar.sh executável
 chmod +x configurar.sh
 chmod +x criar.sh
