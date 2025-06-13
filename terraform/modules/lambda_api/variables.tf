783efaec6d7ed52e98f76ebf99aa877ba8ef1f39
tonanuvem <contato@tonanuvem.net>
2025-06-13 02:10:41 -0300

    atualizando

--- terraform/modules/lambda_api/variables.tf
+++ terraform/modules/lambda_api/variables.tf
@@ -1,19 +1,3 @@
-variable "lambda_nome_dominio" {
-  description = "Nome do domínio base (ex: lab.tonanuvem.com)"
-  type        = string
-}
-
-variable "lambda_id_zona_hospedada" {
-  description = "ID da zona hospedada no Route 53"
-  type        = string
-}
-
-variable "lambda_senha_compartilhada" {
-  description = "Senha compartilhada para autenticação"
-  type        = string
-  sensitive   = true
-}
-
 variable "lambda_tags" {
   description = "Tags padrão para todos os recursos do módulo Lambda"
   type        = map(string)
